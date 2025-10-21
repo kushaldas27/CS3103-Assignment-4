c0d1d3389b6f7f89b1caf9ac10aa4e996b1addda
Derrick Wong <142132416+LemonDrew@users.noreply.github.com>
2025-10-21 15:07:59 +0800

    Finish basic layout for sender.py

--- sender.py
+++ sender.py
@@ -8,11 +8,18 @@ def main():
         isReliable = random.randint(0,1) # Randomizer to determine between 0 (Unreliable) and 1 (Reliable)
         data = "fwfwef" # Hardcoded data
 
-        packet = Packet(data, isReliable)
+        # TODO
+        # packet construction shd be sent to gameNetAPI
 
-        print("P")
+        packet = Packet(data, isReliable) 
 
-
+        print(f"---------Packet {i}-----------")
+        print("Packet ID :", packet.getPacketId())
+        print("Created at :", packet.getTimeStamp())
+        print("Assigned Channel :", packet.getChannel())
+        print("Contained Data :", packet.getData())
+        print("Bytes format :")
+        
     return
 
 
