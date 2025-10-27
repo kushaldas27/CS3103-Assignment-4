0aa9e5e55349165cb7ac2fbbb7a2ea25e8f62bc1
Derrick Wong <142132416+LemonDrew@users.noreply.github.com>
2025-10-27 17:45:36 +0800

    Add logic to wrap data in Packet class

--- gameNetClient-demo.py
+++ gameNetClient-demo.py
@@ -10,11 +10,11 @@ async def main():
     #Upon running, client will randomize stream id and sent random data which will be encapsulated in Packet Class
 
     while True:
-        client.ping()
+        client.run()
         await asyncio.sleep(1)  # wait 1 second or else it will overwhelm the server
 
-        client.send_data(client.reliable_stream, b"This is a reliable message")
-        await asyncio.sleep(1)
+        # client.send_data(client.reliable_stream, b"This is a reliable message")
+        # await asyncio.sleep(1)
 
 
 asyncio.run(main())
