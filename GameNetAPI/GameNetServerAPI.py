d598e8591c690fdbb311dfcbfae8a7f86784911e
Derrick Wong <142132416+LemonDrew@users.noreply.github.com>
2025-10-31 11:44:36 +0800

    Refactor stream handling

--- GameNetAPI/GameNetServerAPI.py
+++ GameNetAPI/GameNetServerAPI.py
@@ -11,61 +11,54 @@ class GameNetServerProtocol(QuicConnectionProtocol):
         super().__init__(*args, **kwargs)
         # buffer per stream for partial reads
         self.stream_buffers = {}
+
+
+    def handleReliableStream(self, event: events.QuicEvent):
+        
+        buf = self.stream_buffers.get(event.stream_id, b"") + event.data
+        while len(buf) >= 4:  # need at least 4 bytes for length
+            packet_len = struct.unpack("!I", buf[:4])[0]
+            if len(buf) < 4 + packet_len:
+                break  # wait for more data
+            packet_bytes = buf[4:4+packet_len]
+            try:
+                packet = pickle.loads(packet_bytes)
+                print(f"Server received stream packet {packet.getPacketId()}: {packet.getData()} on stream {event.stream_id}")
+            except pickle.UnpicklingError:
+                print(f"[Stream {event.stream_id}] Invalid packet")
+            buf = buf[4+packet_len:]
+
+        self.stream_buffers[event.stream_id] = buf
+
+        self.logPackets()
+
+        if event.data == b"PING": # Retransmission 
+            self._quic.send_stream_data(event.stream_id, b"PONG", end_stream=False)
+        else:
+            self._quic.send_stream_data(event.stream_id, b"ACK", end_stream=False)
+        self.transmit()
+
+    
+    def handleUnreliableStream(self, event: events.QuicEvent):
+        try:
+            packet = self.analyzePacket(event.data)
+            print(f"Server received datagram packet {packet.getPacketId()}: {packet.getData()}")
+        except pickle.UnpicklingError:
+            print("Dropped invalid datagram packet")
         
     def quic_event_received(self, event: events.QuicEvent): # Action to take upon receiving events
         if isinstance(event, events.StreamDataReceived):
-            
-            if isinstance(event, events.StreamDataReceived):
-                buf = self.stream_buffers.get(event.stream_id, b"") + event.data
+            self.handleReliableStream(event)
 
-            while len(buf) >= 4:  # need at least 4 bytes for length
-                packet_len = struct.unpack("!I", buf[:4])[0]
-                if len(buf) < 4 + packet_len:
-                    break  # wait for more data
-                packet_bytes = buf[4:4+packet_len]
-                try:
-                    packet = pickle.loads(packet_bytes)
-                    print(f"Server received stream packet {packet.getPacketId()}: {packet.getData()} on stream {event.stream_id}")
-                except pickle.UnpicklingError:
-                    print(f"[Stream {event.stream_id}] Invalid packet")
-                buf = buf[4+packet_len:]
-
-            self.stream_buffers[event.stream_id] = buf
-            
-            # Deparse Packet Class 
-            # packet = self.analyzePacket(event.data)
-            print(f"Server received stream packet {packet.getPacketId()}: {packet.getData()} on stream {event.stream_id}")
-            # print(f"Server received on stream {event.stream_id}: {packet.getData()}")
-            
-            #TODO add logging (eg count latency eg)
-            self.logPackets()
-
-            if event.data == b"PING": # Retransmission 
-                
-                #TODO Set retransmission
-
-                self._quic.send_stream_data(event.stream_id, b"PONG", end_stream=False)
-            else:
-                # TODO: change content of ACK according to application logic, like application sequence numbers
-
-                self._quic.send_stream_data(event.stream_id, b"ACK", end_stream=False)
-
-            self.transmit()
-        
         elif isinstance(event, events.DatagramFrameReceived):
-            # print(f"Server received datagram: {event.data}")
-
-            try:
-                packet = self.analyzePacket(event.data)
-                print(f"Server received datagram packet {packet.getPacketId()}: {packet.getData()}")
-            except pickle.UnpicklingError:
-                print("Dropped invalid datagram packet")
+            self.handleUnreliableStream(event)
+            
 
     def analyzePacket(self, packet_bytes):
         packet_object = pickle.loads(packet_bytes)
         return packet_object
 
-    def logPackets(self, packet):
+    def logPackets(self):
         return
 
 class GameNetServer:
@@ -80,7 +73,6 @@ class GameNetServer:
         except Exception:
             raise RuntimeError("Server TLS cert/key not found or invalid: place cert.pem and key.pem in the project directory")
 
-        # protocol used to handle incoming events
         self.protocol = GameNetServerProtocol
 
         self.server = None
